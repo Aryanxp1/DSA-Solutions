592c9db574e5d77b8fb324f245723c9700c996ab
Aryan <vishwakarmaaryan606@gmail.com>
2026-08-21 00:06:39 +0530

    Implement kthCharacter method to generate sequence

---
+++ 3304.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        word = "a"
+
+        while len(word) < k:
+            new_word = ""
+
+            for ch in word:
+                if ch == "z":
+                    new_word += "a"
+                else:
+                    new_word += chr(ord(ch) + 1)
+
+            word += new_word
+
+        return word[k - 1]
